fbfde2998fa66286d833eee3ea29916294c6d1d9
Anderson Ferrigo Silva <anderson.ferrigo@yahoo.com>
2019-01-29 22:37:33 -0200

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/clearsys/professorama/api/com/clearsys/professoramaws/api/ProfessoramaApiApplication.java
@@ -0,0 +1,14 @@
+package com.clearsys.professorama.api.com.clearsys.professoramaws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessoramaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessoramaApiApplication.class, args);
+	}
+
+}
+
